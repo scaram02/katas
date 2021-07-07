b5bdb1406d026ab15acc19da57c08ff720e54bb3
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-07-07 15:16:12 -0500

    add start vowels-back kata

---
+++ 6-kyu-vowels-back.js
@@ -0,0 +1,9 @@
+function vowelBack(s){
+  const vows = 'aeiou'.split('')
+  const alpha = "abcdefghijklmnopqrstuvwxyz".split('')
+  
+  const trans = s.split('').map((l) => {
+    const replaceWith = alpha.indexOf(l)
+    // vows.includes(i)? s.replace(l, )
+  })
+}
