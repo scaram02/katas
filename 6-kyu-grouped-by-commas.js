026ce051a5b9f6787088faef8b8123505cb35a49
Amelia Scarbrough <scarbroughm@hotmail.com>
2021-07-22 23:26:48 -0500

    added to kata but still need to fix

--- 6-kyu-grouped-by-commas.js
+++ 6-kyu-grouped-by-commas.js
@@ -12,3 +12,24 @@
 //  1000000  ->   "1,000,000"
 // 35235235  ->  "35,235,235"
 
+function groupByCommas(n) {
+
+    let numOfTrailingZeros = n.toString().lastIndexOf('0') - n.toString().indexOf('0') + 1 // if has zeroes is above 1
+  console.log("num trainl", numOfTrailingZeros)
+  let string;
+  if (numOfTrailingZeros > 1){
+    string = n.toString().split('').reverse().join('')
+    } else {
+      string = n.toString()
+    }
+    
+   const almost =  string.split('').map((digit, index) => {
+      return (index > 0 && index % 3 === 0)? `,${digit}` : digit
+    }).join('')
+  
+  return almost.split(',').reverse().join(',')
+  
+  
+    
+  }
+  groupByCommas(10000)
